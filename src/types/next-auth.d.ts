1d4b16c8306b81a61632cdfda7a4e9c670731db6
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-16 13:57:37 +0100

    next auth typings + setup done

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+type = userId = string;
+
+import type { Session, User } from "next-auth";
+import type { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: userId;
+    username?: string | null;
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    id: userId;
+    username?: string | null;
+  }
+}
