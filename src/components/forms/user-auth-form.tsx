17b681fa5fbcbeedb383913045f999f8c41f504b
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-16 11:03:53 +0100

    install toast component + shadcn setup

--- src/components/forms/user-auth-form.tsx
+++ src/components/forms/user-auth-form.tsx
@@ -1,12 +1,38 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { Icons } from "@/components/icons";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: React.FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn("google");
+    } catch (error) {
+      // toast notification
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className={cn("flex justify-center", className)} {...props}>
-      <Button>Google</Button>
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        size="sm"
+        className="w-full"
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
+        Google
+      </Button>
     </div>
   );
 };
